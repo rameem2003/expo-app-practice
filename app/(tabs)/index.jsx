19f67013e38e5bf6e12d270492518aa74e521bfb
rameem2003 <rameem2019@gmail.com>
2025-03-13 10:04:32 +0600

    first commit

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Tab [Home|Settings]</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ app/index.jsx
@@ -0,0 +1,112 @@
+import { Link, Redirect } from "expo-router";
+import { useEffect } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        // justifyContent: "center",
+        // alignItems: "center",
+        backgroundColor: "#FFFFFF",
+        paddingHorizontal: 32,
+        paddingVertical: 19,
+      }}
+    >
+      <Text style={{ fontSize: 36, fontWeight: "bold" }}>Welcome</Text>
+      <Text style={{ fontSize: 36, fontWeight: "bold" }}>Back</Text>
+      {/* <Link href={"/(tabs)"}>Go</Link> */}
+
+      <View style={{ marginTop: 36 }}>
+        <View style={{ position: "relative" }}>
+          <Feather
+            style={{
+              position: "absolute",
+              top: "50%",
+              zIndex: 10,
+              left: 10,
+              transform: "translateY(-15%)",
+            }}
+            name="user"
+            size={24}
+            color="black"
+          />
+          <TextInput
+            style={{
+              padding: 20,
+              borderColor: "#A8A8A9",
+              borderWidth: 1,
+              borderRadius: 10,
+              backgroundColor: "#F3F3F3",
+              paddingLeft: 38,
+            }}
+            placeholder="Username or Email"
+          />
+        </View>
+        <View style={{ position: "relative", marginTop: 31 }}>
+          <Feather
+            style={{
+              position: "absolute",
+              top: "50%",
+              zIndex: 10,
+              left: 10,
+              transform: "translateY(-15%)",
+            }}
+            name="lock"
+            size={24}
+            color="black"
+          />
+          <TextInput
+            style={{
+              padding: 20,
+              borderColor: "#A8A8A9",
+              borderWidth: 1,
+              borderRadius: 10,
+              backgroundColor: "#F3F3F3",
+              paddingLeft: 38,
+            }}
+            secureTextEntry={true}
+            placeholder="Password"
+          />
+        </View>
+        <Text style={{ textAlign: "right", marginTop: 10, color: "#F83758" }}>
+          Forgot Password?
+        </Text>
+
+        <TouchableOpacity
+          style={{
+            padding: 15,
+            backgroundColor: "#F83758",
+            marginTop: 52,
+            borderRadius: 4,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "#fff",
+              fontSize: 20,
+              fontWeight: "semibold",
+            }}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+
+        <View style={{ marginTop: 194 }}>
+          <Text style={{ textAlign: "center", fontSize: 14 }}>
+            Create An Account{" "}
+            <Link
+              href={"/signup"}
+              style={{ fontWeight: "semibold", color: "#F83758" }}
+            >
+              Sign Up
+            </Link>
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
